123c291ccf57d066bdfaaecd98c572f706a62cc3
mirshodbek <mirshodbek2003@gmail.com>
2023-05-11 10:31:12 +0500

    add product app  and implimentations

--- product/serializers.py
+++ product/serializers.py
@@ -2,12 +2,18 @@ from rest_framework.serializers import ModelSerializer
 from .models import Category,Product
 
 
-class CategorySerializer(ModelSerializer):
+class ChildCategorySerializer(ModelSerializer):
 
     class Meta:
         model = Category
         fields = "__all__"
 
+class CategorySerializer(ModelSerializer):
+    child_category_obj = ChildCategorySerializer(source="category_set", many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = "__all__"
 
 class ProductSerializer(ModelSerializer):
     class Meta:
